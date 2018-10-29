b69d2dd0002fabba889679da8065f254ea32d213
Stein121 <qscaxz0@gmail.com>
2018-10-29 23:19:17 +0800

    Draw ticks

--- drawAxes.py
+++ drawAxes.py
@@ -11,7 +11,7 @@ turtle.setup(width=530, height=530, startx=0, starty=0) # add 30 for padding
 turtle.screensize(canvwidth=500, canvheight=500)
 turtle.title("Draw Axes")
 
-# draw x axis
+# move turtle
 turtle.penup()
 turtle.setpos(-250, -250)
 turtle.pendown()
@@ -29,5 +29,32 @@ turtle.right(150)
 turtle.forward(5)
 turtle.left(180)
 turtle.forward(5)
+
+# move turtle
+turtle.penup()
+turtle.setpos(-250, -250)
+turtle.pendown()
+
+# draw the ticks on the axis
+turtle.seth(0)
+turtle.forward(500/10/2)
+for i in range(9):
+	turtle.left(90)
+	turtle.forward(3)
+	turtle.left(180)
+	turtle.forward(6)
+	turtle.left(180)
+	turtle.forward(3)
+	turtle.right(90)
+	turtle.forward(500/10)
+
+# last tick
+turtle.left(90)
+turtle.forward(3)
+turtle.left(180)
+turtle.forward(6)
+turtle.left(180)
+turtle.forward(3)
+turtle.right(90)
 
 turtle.exitonclick()
